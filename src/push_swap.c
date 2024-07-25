2a6c8c2ad5aecd56f50a5d3bdab998b57a1c0465
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-07-25 19:55:50 +0200

    push_swap structs a medias

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 12:40:16 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/07/25 16:47:09 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/07/25 19:24:09 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 int main(int argc,char **argv)
 {
-	t_data data;
-
-	init_data(argc, argv, &data, true);
+	char	**nbr;
+	t_list	*a;
+	t_list	*b;
+	
+	if (argc > 1)
+	init_data(argc, argv);
 }
