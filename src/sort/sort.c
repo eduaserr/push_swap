499833b3bdac11da92ac5b230fbd2713f1c198b0
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-09-02 21:57:38 +0200

    limits stacks function added

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,12 +6,54 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 13:39:55 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/08/30 14:06:32 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/09/02 21:55:11 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/libft.h"
 #include "../inc/push_swap.h"
+
+void	limits_stack(int *min, int *max, t_stack *stack)
+{
+	int	i;
+	int	min_value;
+	int	max_value;
+
+	i = 1;
+	*min = 1;
+	*max = 1;
+	min_value = stack->value;
+	max_value = stack->value;
+	stack = stack->next;
+	while (stack)
+	{
+		i++;
+		if (min_value > stack->value)
+		{
+			min_value = stack->value;
+			*min = i;
+		}
+		if (max_value < stack->value)
+		{
+			max_value = stack->value;
+			*max = i;
+		}
+		stack = stack->next;
+	}
+}
+
+void	sort_three(t_stack **stack)
+{
+	int	min;
+	int	max;
+	int	i;
+
+	i = 0;
+	limits_stack(&min, &max, *stack);
+}
+
+void	sort_big()
+{}
 
 void	sort(t_stack **a, t_stack **b)
 {
@@ -20,10 +62,8 @@ void	sort(t_stack **a, t_stack **b)
 	if (ft_stacklen(*a) == 2)
 		sa(*a);
 	else if (ft_stacklen(*a) == 3)
-	{
 		while (!(is_sorted(*a)))
 			sort_three(a);
-	}
 	else if (!is_sorted(*a))
 		sort_big();
 }
