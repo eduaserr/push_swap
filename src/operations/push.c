27b9dacc9e00c16a81550d4d4fe592ed4e89f755
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-09-04 20:17:35 +0200

    added push functions

--- src/operations/push.c
+++ src/operations/push.c
@@ -6,12 +6,33 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:48:08 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/09/03 20:48:52 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/09/04 20:16:20 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/libft.h"
 #include "../inc/push_swap.h"
 
-void	push()
-{}
+void	push(t_stack **src, t_stack **dst)
+{
+	t_stack *push_node;
+
+	if (!(*src))
+		return ;
+	push_node = *src;
+	*src = (*src)->next;
+	push_node->next = *dst;
+	*dst = push_node;
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	ft_printf("pa\n");
+	push(b, a);
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	ft_printf("pb\n");
+	push(a, b);
+}
