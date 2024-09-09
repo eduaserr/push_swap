d6e82f805ba18b0feeaffe1e98e4cb37ce5971fe
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-09-09 20:14:34 +0200

    handle error new_nde if malloc fails

--- src/stack/init.c
+++ src/stack/init.c
@@ -6,7 +6,7 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 19:09:11 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/09/05 21:50:32 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/09/09 20:11:07 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	init_stack(t_stack **a, long n)
 	t_stack	*new_node;
 
 	new_node = ft_lstnew_ps(n);
+	if (!new_node)
+	{
+		free_stack(a);
+		p_error();
+	}
 	if (!(*a))
 		*a = new_node;
 	else
